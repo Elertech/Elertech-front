7c7a5744153ff40ca988f7ff8a24685484206e8b
ViniciusCampanholi <viniciuscampanholi@hotmail.com>
2022-07-27 15:59:08 -0300

    Pedido

--- src/app/components/historico-pedidos/historico-pedidos.component.ts
+++ src/app/components/historico-pedidos/historico-pedidos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Item } from 'src/app/model/Item';
+import { ItemPedido } from 'src/app/model/ItemPedido';
 import { Pedido } from 'src/app/model/Pedido';
 import { AuthService } from 'src/app/service/auth.service';
 import { PedidoService } from 'src/app/service/pedido.service';
@@ -11,9 +11,9 @@ import { PedidoService } from 'src/app/service/pedido.service';
 })
 export class HistoricoPedidosComponent implements OnInit {
 
-  listaPedidos: Pedido[] = []
+  listaPedidos = new Array()
   pedido: Pedido = new Pedido()
-  listaItenPedido: Item[] = []
+  listaItenPedido = new Array()
   pagamentoPedido: string
   enderecoEntregaPedido: string
 
@@ -28,13 +28,25 @@ export class HistoricoPedidosComponent implements OnInit {
   getPedidos(){
     this.pedidoService.getAllByUsuario().subscribe((data: Pedido[])=>{
       this.listaPedidos = data
+
+      this.listaPedidos.forEach(pedido => {
+        pedido.valorTotalPedido = pedido.valorTotalPedido.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+      })
     })
   }
 
-  abrirPedido(pedido: Pedido){
-    this.listaItenPedido = pedido.item
-    this.enderecoEntregaPedido = pedido.enderecoEntrega
-    this.pagamentoPedido = pedido.formaPagamento
+  abrirPedido(id: number){
+    this.pedidoService.getPedidoById(id).subscribe((data: Pedido)=>{
+      this.pedido = data
+      this.listaItenPedido = this.pedido.item
+
+      this.listaItenPedido.forEach(i => {
+        i.produto.preco = i.produto.preco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+        i.valorTotal = i.valorTotal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+      })
+      this.enderecoEntregaPedido = this.pedido.enderecoEntrega
+      this.pagamentoPedido = this.pedido.formaPagamento
+    })
   }
 
 }
